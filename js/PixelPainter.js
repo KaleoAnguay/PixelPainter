a8ea9eae9343e3123b96e959997aae46d5e57e37
Michael <michaelanguay@outlook.com>
2017-03-25 16:37:42 -1000

    added color all and random color

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,18 +1,20 @@
 //create Grid to color.  Append buttons to Grid.  Buttons are the best!
 //painter set
 
-var pixgrid = document.getElementById('pixelPainter');
-var colorGrid = document.getElementById('pixelPainter');
-var paint = null;
+var pixgrid = document.querySelector('#pixelPainter');
+var paint = 'white';
+var painted = function() {
+  event.target.style.background = paint;
+};
 
 var whiteBox = document.createElement('div');
 
 whiteBox.id = 'Whitebox';
-whiteBox.style.width = '500px';
+whiteBox.style.width = '650px';
 whiteBox.style.height = '500px';
 pixgrid.appendChild(whiteBox);
 
-var buttnum = 400;
+var buttnum = 520;
 var isMouseDown = false;
 
 
@@ -22,6 +24,7 @@ for(var i = 0; i < buttnum; i++) {
   button.style.background = "white";
   button.style.width = '25px';
   button.style.height = '25px';
+  button.style.cursor = 'crosshair';
 
   button.addEventListener("mousedown", function(event){
     isMouseDown = true;
@@ -47,7 +50,7 @@ var colorButtons = document.createElement('div');
 colorButtons.id = 'color';
 colorButtons.style.width = '200px';
 colorButtons.style.height = '200px';
-colorGrid.appendChild(colorButtons);
+pixgrid.appendChild(colorButtons);
 
 //Color Wheel array to iterate through and number of buttons
 var colorbutt = 12;
@@ -60,6 +63,7 @@ for(var i = 0; i <colorbutt; i++) {
   buttoncolor.style.background = colorsOfTheRainbow[i];
   buttoncolor.style.width  = '50px';
   buttoncolor.style.height = '50px';
+  buttoncolor.style.cursor = 'crosshair';
   buttoncolor.addEventListener('click', function(event) {
    console.log(event.target.id);
    paint = event.target.id;
@@ -67,4 +71,39 @@ for(var i = 0; i <colorbutt; i++) {
   });
   colorButtons.appendChild(buttoncolor);
 }
-// Creating paint variable to color with.
+
+// Color all of pixpainter
+var colorAll = document.createElement('div');
+  colorAll.id = 'All';
+  colorAll.style.width = '500px';
+  colorAll.style.height = '500px';
+  pixgrid.appendChild(colorAll);
+
+//Random Colors and Full color stack
+var randomButt = 2;
+
+for(var i = 0; i<randomButt; i++) {
+  var instColor = document.createElement('button');
+    instColor.id = 'instant' + i;
+    instColor.style.width = '100px';
+    instColor.style.height = '50px';
+    instColor.addEventListener('click', function(event) {
+      console.log(event.target.id);
+  });
+    colorAll.appendChild(instColor);
+}
+//Query selector all returns an array.
+//loop through it in order to access the buttons.
+var everything = whiteBox.querySelectorAll('button');
+
+instant0.innerHTML = "Color All";
+instant1.innerHTML = "Random Color";
+
+instant0.addEventListener('click', function(event) {
+  everything.forEach(painted);
+});
+
+
+
+
+
